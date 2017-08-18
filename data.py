46611ab0a99f1d1653a0a9b130d7c91f8ed98a81
anhkt <anhkt1603@gmail.com>
2017-08-18 11:30:48 +0700

    edit-delete

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+	articles = [
+		{
+			'id':1,
+			'title':'Article One',
+			'body':'when the application runs we should show a home page',
+			'author':'Michale',
+			'created_date':'16-08-2017'
+		},
+		{
+			'id':2,
+			'title':'Article Two',
+			'body':'when the application runs we should show a home page',
+			'author':'Michale',
+			'created_date':'16-08-2017'
+		},
+		{
+			'id':3,
+			'title':'Article Three',
+			'body':'when the application runs we should show a home page',
+			'author':'Michale',
+			'created_date':'16-08-2017'
+		}
+	]
+
+	return articles
+	
